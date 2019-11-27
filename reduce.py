8a0caf9e172bd1f65b54387aac99231b52e8ab4c
vlysenok <tina.lysenok@gmail.com>
2019-11-27 12:51:20 +0200

    hw8_py_reduce

--- reduce.py
+++ reduce.py
@@ -1,6 +1,25 @@
-from typing import Callable, Iterable, Any, List
+"""
+module reduce
+"""
+
+from typing import Callable, Iterable, Any
 
 
-def reduce_(func: Callable[[Any, Any], Any], l: Iterable[Any]) -> Any:
-
-    return 42
+def reduce_(func: Callable[[Any, Any], Any], lst: Iterable[Any], initializer=None) -> Any:
+    """
+    Implementation of filter() function.
+    :param func: Callable[[Any, Any], Any]
+    :param lst: Iterable[Any]
+    :param initializer: None
+    :return: Any
+    """
+    iter_ = iter(lst)
+    if initializer is None:
+        try:
+            initializer = next(iter_)
+        except StopIteration:
+            raise TypeError('reduce() of empty sequence with no initial value')
+    accum_value = initializer
+    for element in iter_:
+        accum_value = func(accum_value, element)
+    return accum_value

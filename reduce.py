8903c0326de6c4afcb2e66308b8d3fa2217d6986
alexander <earthhuman41@gmail.com>
2019-11-27 09:43:48 +0200

    implemented reduce

--- reduce.py
+++ reduce.py
@@ -1,6 +1,20 @@
-from typing import Callable, Iterable, Any, List
+"""
+reduce
+"""
+from typing import Callable, Any, List
 
 
-def reduce_(func: Callable[[Any, Any], Any], l: Iterable[Any]) -> Any:
+def reduce_(
+        func: Callable[[Any, Any], Any], list_to_reduce: List[Any]
+) -> Any:
+    """
 
-    return 42
+    :param func: callback
+    :param list_to_reduce: list
+    :return: accumulator
+    """
+    accumulator = list_to_reduce[0]
+    for i in range(1, len(list_to_reduce)):
+        accumulator = func(accumulator, list_to_reduce[i])
+
+    return accumulator

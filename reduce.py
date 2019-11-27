c89d217efa484299d49e7daa4ce0d9bffa6f6da7
logotip <logotip123@yahoo.com>
2019-11-27 11:31:47 +0200

    v 1.0

--- reduce.py
+++ reduce.py
@@ -1,6 +1,19 @@
-from typing import Callable, Iterable, Any, List
+"""reduce function module"""
+from typing import Callable, Iterable, Any
 
 
-def reduce_(func: Callable[[Any, Any], Any], l: Iterable[Any]) -> Any:
-
-    return 42
+def reduce_(func: Callable[[Any, Any], Any], objects: Iterable[Any]) -> Any:
+    """
+    reduce function
+    :param func: func to reduce
+    :param objects: iterable object
+    :return: result
+    """
+    if not objects:
+        return False
+    objects = list(objects)
+    while len(objects) > 1:
+        result = func(objects[0], objects[1])
+        objects = objects[1:]
+        objects[0] = result
+    return objects[0]

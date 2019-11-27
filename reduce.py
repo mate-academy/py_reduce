0114f5c20752a74d1c1719d3d592aca8d6cad226
YuPo13 <yuliya.povzlo@gmail.com>
2019-11-27 18:29:35 +0200

    Coded

--- reduce.py
+++ reduce.py
@@ -1,6 +1,10 @@
-from typing import Callable, Iterable, Any, List
+"""module docstring"""
+#from typing import Callable, Iterable, Any, List
 
 
-def reduce_(func: Callable[[Any, Any], Any], l: Iterable[Any]) -> Any:
-
-    return 42
+def reduce_(func, listing):
+    """func docstring"""
+    result = listing[0]
+    for i in listing[1:]:
+        result = func(result, i)
+    return result

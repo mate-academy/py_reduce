6f22543a2c95f624d8ee4e2835d5c040e64244db
DukhDmytro <cowboybebop4991@gmail.com>
2019-11-27 11:13:39 +0200

    1st commit

--- reduce.py
+++ reduce.py
@@ -1,6 +1,10 @@
-from typing import Callable, Iterable, Any, List
+"""import"""
+from typing import Callable, Any
 
 
-def reduce_(func: Callable[[Any, Any], Any], l: Iterable[Any]) -> Any:
-
-    return 42
+def reduce_(func: Callable[[Any, Any], Any], lst) -> Any:
+    """reduce"""
+    start = lst[0]
+    for i in lst[1:]:
+        start = func(start, i)
+    return start

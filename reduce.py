b560e05d9310d7157c3b9951f88bb20084b45a77
Artem <artger@ukr.net>
2019-11-27 16:14:22 +0200

    version 2

--- reduce.py
+++ reduce.py
@@ -1,7 +1,16 @@
+'''
+Module
+'''
 from typing import Callable, Iterable, Any
 
 
-def reduce_(func: Callable[[Any, Any], Any], array: Iterable[Any]) -> Any:
+def reduce_(func: Callable[[Any, Any], Any], array) -> Any:
+    '''
+
+    :param func:
+    :param array:
+    :return:
+    '''
     result = array[0]
     for i in array[1:]:
         result = func(result, i)

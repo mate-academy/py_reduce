4992b1e290ff5147e77eec0ac6b12de92830cbdb
koreyo <KoretsValentyn@gmail.com>
2019-12-02 00:39:14 +0200

    hw

--- reduce.py
+++ reduce.py
@@ -1,6 +1,12 @@
-from typing import Callable, Iterable, Any, List
+"""docstring"""
+from typing import Callable, Any
 
 
-def reduce_(func: Callable[[Any, Any], Any], l: Iterable[Any]) -> Any:
-
-    return 42
+def reduce_(func: Callable[[Any, Any], Any], list_item) -> Any:
+    """my version reduce function"""
+    if not list_item:
+        return False
+    res = list_item[0]
+    for i in list_item[1:]:
+        res = func(res, i)
+    return res

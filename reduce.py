1bff1142973e696f02d5dfba80aa5c0875828d37
Platoniuks <platon@alterpost.org>
2019-12-01 12:47:01 +0200

    Own implementation of reduce func

--- reduce.py
+++ reduce.py
@@ -1,6 +1,10 @@
-from typing import Callable, Iterable, Any, List
+"""Own implementation reduce func"""
 
-
-def reduce_(func: Callable[[Any, Any], Any], l: Iterable[Any]) -> Any:
-
-    return 42
+def reduce_(func, arr):
+    """Apply function of two arguments cumulatively to the items of iterable,
+     from left to right, so as to reduce the iterable to a single value.
+     """
+    result = arr[0]
+    for i in range(1, len(arr)):
+        result = func(result, arr[i])
+    return result

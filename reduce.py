a8d94dac0185a07ef88d0d0d8fd70eadf709cc64
Andrii <andrewsotnikov88@gmail.com>
2019-12-01 18:57:55 +0200

    Created custom reduce function

--- reduce.py
+++ reduce.py
@@ -1,6 +1,11 @@
-from typing import Callable, Iterable, Any, List
+"""Create your own implementation of filter() function."""
+from typing import Callable, Iterable, Any
 
 
-def reduce_(func: Callable[[Any, Any], Any], l: Iterable[Any]) -> Any:
-
-    return 42
+def reduce_(func: Callable[[Any, Any], Any], array) -> Any:
+    """Apply function of two arguments cumulatively to the items of iterable,
+     from left to right, so as to reduce the iterable to a single value."""
+    total = array[0]
+    for _ in array[1::]:
+        total = func(total, _)
+    return total
